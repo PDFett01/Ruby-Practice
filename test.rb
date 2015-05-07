3cd519bb34c1d7711714ed361cfbebac52c3fc7a
Patrick Fette <pdfett01@gmail.com>
2015-05-07 10:58:34 -0400

    Test Program
    This program is a test of the get and put functions. It is being used to
    configure Ruby on Windows to work with Github and Notepad++

---
+++ test.rb
@@ -0,0 +1,10 @@
+print "Please enter your name: "
+name = gets
+
+print "Please enter your age: "
+age = gets
+
+print "What is your favorite color: "
+color = gets
+
+puts "Hello #{name}Aged #{age}Color Preference #{color}"
