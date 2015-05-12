e03651a580f59f70e7eb7232373f1c00ce4e87c1
Patrick Fette <pdfett01@gmail.com>
2015-05-12 18:34:45 -0400

    Mad Libs program
    Basic program that uses Gets, Puts, and Prints as practice

---
+++ madlibs.rb
@@ -0,0 +1,38 @@
+print "Enter an adjective: "
+adjective = gets.chomp
+
+print "Enter a noun: "
+noun = gets.chomp
+
+print "Enter a verb in the past tense: "
+verbpt = gets.chomp
+
+print "Enter an adverb: "
+adverb = gets.chomp
+
+print "Enter another adjective: "
+second = gets.chomp
+
+print "Enter another noun: "
+dog = gets.chomp
+
+print "Enter one more noun: "
+cat = gets.chomp
+
+print "Enter a third adjective: "
+third = gets.chomp
+
+print "Enter a verb: "
+verb = gets.chomp
+
+print "Enter another adverb: "
+ad = gets.chomp
+
+print "Enter another verb in the past tense: "
+past = gets.chomp
+
+print "Enter a last adjective: "
+fourth = gets.chomp
+
+
+puts "Today I went to the zoo. I saw a #{adjective} #{noun} jumping up and down in its tree. He #{verbpt} #{adverb} through the large tunnel that led to its #{second} #{dog}. I got some peanuts and passed them through the cage to a gigantic gray #{cat} towering above my head. Feeding that animal made me hungry. I went to get a #{third} scoop of ice cream. It filled my stomach. Afterwards I had to #{verb} #{ad} to catch our bus. When I got home I #{past} my mom for a #{fourth} day at the zoo."
